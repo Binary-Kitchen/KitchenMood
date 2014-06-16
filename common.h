39c59d71bac2d9d55b474e7df11e26909c5f4551
Ralf Ramsauer <ralf@ramses-pyramidenbau.de>
2014-06-16 20:22:59 +0200

    Added CRC8 support

--- common.h
+++ common.h
@@ -4,7 +4,7 @@
 #define ACTON  PORTE |=  0x08;
 #define ACTOFF PORTE &= ~0x08;
 #define ONON   PORTE |=  0x40;
-#define ONOFF  PORTE |= ~0x40;
+#define ONOFF  PORTE &= ~0x40;
 
 #define PWMCHANNELS 30
 
